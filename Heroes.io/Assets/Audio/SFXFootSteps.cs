9d9d134bb40ff8cc3b3714eda7e950874cfa863b
gkaragoz <gkaragoz96@gmail.com>
2019-06-20 00:33:24 +0300

    Keep adding prefabs and audio scripts. Change project build settings to Android.

---
+++ Heroes.io/Assets/Audio/SFXFootSteps.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SFXFootSteps : MonoBehaviour {
+
+    [SerializeField]
+    private AudioClip[] _clips = null;
+
+    private AudioSource _audioSource;
+
+    private void Awake() {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Step() {
+        AudioClip clip = GetRandomClip();
+        _audioSource.PlayOneShot(clip);
+    }
+    
+    private AudioClip GetRandomClip() {
+        return _clips[Random.Range(0, _clips.Length)];
+    }
+
+}
