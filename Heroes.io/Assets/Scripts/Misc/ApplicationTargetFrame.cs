197dbc1591dbe9414b4eaf78b337604f2b983a31
gkaragoz <gkaragoz96@gmail.com>
2019-06-20 01:34:49 +0300

    Create ApplicationTargetFrame.cs script

---
+++ Heroes.io/Assets/Scripts/Misc/ApplicationTargetFrame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ApplicationTargetFrame : MonoBehaviour {
+
+    [SerializeField]
+    [Range(1, 60)]
+    private int _applicationTargetFrame = 60;
+
+    private void Update() {
+        Application.targetFrameRate = _applicationTargetFrame;
+    }
+
+}
