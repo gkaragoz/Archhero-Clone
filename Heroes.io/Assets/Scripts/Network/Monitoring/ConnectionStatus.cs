edbab808fe51593cf518f0d12423859dd63707a0
gkaragoz <gkaragoz96@gmail.com>
2019-06-20 12:07:18 +0300

    Implement ConnectionStatus text UI for monitoring.

---
+++ Heroes.io/Assets/Scripts/Network/Monitoring/ConnectionStatus.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class ConnectionStatus : MonoBehaviour {
+
+    [SerializeField]
+    private string _connectionStatusMessage = "Connection Status: ";
+
+    [SerializeField]
+    private TextMeshProUGUI _txtConnectionStatus;
+
+    private void Update() {
+        _txtConnectionStatus.text = _connectionStatusMessage + PhotonNetwork.NetworkClientState;
+    }
+
+}
