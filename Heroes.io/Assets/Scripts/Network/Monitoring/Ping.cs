adfa5f70da8bd2413b001762f1254ef2cf0a91e8
gkaragoz <gkaragoz96@gmail.com>
2019-06-20 12:10:41 +0300

    Implement ping monitoring.

---
+++ Heroes.io/Assets/Scripts/Network/Monitoring/Ping.cs
@@ -0,0 +1,29 @@
+﻿using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+public class Ping : MonoBehaviour {
+
+    [SerializeField]
+    private string _pingMessage = "Ping: ";
+
+    [SerializeField]
+    private TextMeshProUGUI _txtPing;
+
+    private int _cache = -1;
+
+    private void Update() {
+        if (PhotonNetwork.IsConnectedAndReady) {
+            if (PhotonNetwork.GetPing() != _cache) {
+                _cache = PhotonNetwork.GetPing();
+                _txtPing.text = _pingMessage + _cache.ToString() + " ms";
+            }
+        } else {
+            if (_cache != -1) {
+                _cache = -1;
+                _txtPing.text = "n/a";
+            }
+        }
+    }
+
+}
